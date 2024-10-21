c775ba319c4a9f105ee4326e0fa3f0f31fe5d7c4
Dario <93393188+dAr104@users.noreply.github.com>
2024-10-21 18:03:05 +0200

    Delete variables directory

--- variables/src/function.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    my_function(5, 6);
-}
-
-fn my_function(x: i32, y: i32) -> i32 {
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-    x + y // return value
-}
--- variables/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    let x = "six";
-    println!("The value of x is: {}", x);
-
-    const SUBSCRIBER_COUNT: u32 = 100_000;
-} 
